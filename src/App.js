980f763c3f108fa6bac183710ca6e9a0b48b3bf5
konain1 <kaunainkasab@gmail.com>
2023-08-11 02:29:04 +0530

    colorSassUpdate

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './Navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
-     
+     <Navbar/>
     </div>
   );
 }
--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -1,12 +1,26 @@
 import React from 'react'
-import "./Navbar.css"
+import "./Navbar.scss"
 
 
 function Navbar() {
   return (
     <div className='navbar'>
 
-    
+    <div className='navbar__logo'>logoSection</div>
+    <div className='navbar__nav'>
+    <ul>
+    <li>English</li>
+    <li>DSA</li>
+    <li>FrontEnd</li>
+    <li>BackEnd</li>
+
+
+
+    </ul>
+
+
+    </div>
+    <div className='navbar__login'>login</div>
       
     </div>
   )
